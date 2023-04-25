297ea7679e43f8745181f15efcc39343d165e49c
Romain Cambonie <romain.cambonie@gmail.com>
2023-04-25 16:40:46 +0200

    feat: trying less destructive way to get template

--- modules/github-repository/actions-workflows-files.tf
+++ modules/github-repository/actions-workflows-files.tf
@@ -5,7 +5,7 @@
 resource "github_repository_file" "s3_sync_and_invalidate_cache" {
   repository          = github_repository.repository.name
   branch              = github_branch_default.main.branch
-  file                = ".github/workflows/autodeploy-clone-template.aws.yml"
+  file                = ".github/workflows/rebase-from-template.aws.yml"
   content             = module.workflow_template.rendered
   commit_message      = "feat: adding build s3 sync and invalidate cache workflow"
   commit_author       = var.commit_author_name
@@ -16,7 +16,7 @@ resource "github_repository_file" "s3_sync_and_invalidate_cache" {
 
 module "workflow_template" {
   source       = "github.com/codingones/terraform-remote-template-renderer"
-  template_url = "https://raw.githubusercontent.com/codingones/github-files-templates/main/github-actions/autodeploy-clone-template.aws.yml"
+  template_url = "https://raw.githubusercontent.com/codingones/github-files-templates/main/github-actions/rebase-from-template.aws.yml"
   template_variables = {
     SERVICE      = var.service
     ORGANIZATION = var.github_organization
